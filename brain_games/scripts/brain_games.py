4c8eff2c4f3281db34df9e4557b41203f5d9bd74
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2025-05-16 10:23:36 +0300

    is_even fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,8 @@
 import random
 
-from brain_games.cli import welcome_user
 from brain_games.games.brain_even import is_even
 
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()
     is_even()
